5e4274431bb8fa3b93ea863b7a4faf48bb9f4aa7
Tim McMackin <timothymcmackin@yahoo.com>
2023-10-03 10:54:25 -0400

    html section of mintpage

--- app/src/MintPage.tsx
+++ app/src/MintPage.tsx
@@ -17,6 +17,147 @@ export default function MintPage() {
 
   return (
     <Paper>
+
+      {storage ? (
+        <Button
+          disabled={storage.administrators.indexOf(userAddress! as address) < 0}
+          sx={{
+            p: 1,
+            position: "absolute",
+            right: "0",
+            display: formOpen ? "none" : "block",
+            zIndex: 1,
+          }}
+          onClick={toggleDrawer(!formOpen)}
+        >
+          {" Mint Form " +
+            (storage!.administrators.indexOf(userAddress! as address) < 0
+              ? " (You are not admin)"
+              : "")}
+          <OpenWithIcon />
+        </Button>
+      ) : (
+        ""
+      )}
+
+      <SwipeableDrawer
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        anchor="right"
+        open={formOpen}
+        variant="temporary"
+      >
+        <Toolbar
+          sx={
+            isTablet
+              ? { marginTop: "0", marginRight: "0" }
+              : { marginTop: "35px", marginRight: "125px" }
+          }
+        />
+        <Box
+          sx={{
+            width: isTablet ? "40vw" : "60vw",
+            borderColor: "text.secondary",
+            borderStyle: "solid",
+            borderWidth: "1px",
+
+            height: "calc(100vh - 64px)",
+          }}
+        >
+          <Button
+            sx={{
+              position: "absolute",
+              right: "0",
+              display: !formOpen ? "none" : "block",
+            }}
+            onClick={toggleDrawer(!formOpen)}
+          >
+            <Close />
+          </Button>
+          <form onSubmit={formik.handleSubmit}>
+            <Stack spacing={2} margin={2} alignContent={"center"}>
+              <Typography variant="h5">Mint a new collection</Typography>
+
+              <TextField
+                id="standard-basic"
+                name="token_id"
+                label="token_id"
+                value={formik.values.token_id}
+                disabled
+                variant="filled"
+              />
+              <TextField
+                id="standard-basic"
+                name="name"
+                label="name"
+                required
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                error={formik.touched.name && Boolean(formik.errors.name)}
+                helperText={formik.touched.name && formik.errors.name}
+                variant="filled"
+              />
+              <TextField
+                id="standard-basic"
+                name="symbol"
+                label="symbol"
+                required
+                value={formik.values.symbol}
+                onChange={formik.handleChange}
+                error={formik.touched.symbol && Boolean(formik.errors.symbol)}
+                helperText={formik.touched.symbol && formik.errors.symbol}
+                variant="filled"
+              />
+              <TextField
+                id="standard-basic"
+                name="description"
+                label="description"
+                required
+                multiline
+                minRows={2}
+                value={formik.values.description}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.description &&
+                  Boolean(formik.errors.description)
+                }
+                helperText={
+                  formik.touched.description && formik.errors.description
+                }
+                variant="filled"
+              />
+
+              {pictureUrl ? (
+                <img height={100} width={100} src={pictureUrl} />
+              ) : (
+                ""
+              )}
+              <Button variant="contained" component="label" color="primary">
+                <AddCircleOutlined />
+                Upload an image
+                <input
+                  type="file"
+                  hidden
+                  name="data"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    const data = e.target.files ? e.target.files[0] : null;
+                    if (data) {
+                      setFile(data);
+                    }
+                    e.preventDefault();
+                  }}
+                />
+              </Button>
+
+              <Button variant="contained" type="submit">
+                Mint
+              </Button>
+            </Stack>
+          </form>
+        </Box>
+      </SwipeableDrawer>
+
+
       <Typography variant="h5">Mint your wine collection</Typography>
 
       {nftContratTokenMetadataMap.size != 0 ? (
