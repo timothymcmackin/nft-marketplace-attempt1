a7af9b05db0cc1b12daf45f771f01d9c99b1a30e
Tim McMackin <timothymcmackin@yahoo.com>
2023-10-03 10:57:09 -0400

    mint function

--- app/src/MintPage.tsx
+++ app/src/MintPage.tsx
@@ -22,6 +22,77 @@ import { address, bytes, nat } from "./type-aliases";
 import { char2Bytes } from "@taquito/utils";
 import { TransactionInvalidBeaconError } from "./TransactionInvalidBeaconError";
 
+const { enqueueSnackbar } = useSnackbar();
+
+const mint = async (newTokenDefinition: TZIP21TokenMetadata) => {
+  try {
+    //IPFS
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const requestHeaders: HeadersInit = new Headers();
+      requestHeaders.set(
+        "pinata_api_key",
+        `${import.meta.env.VITE_PINATA_API_KEY}`
+      );
+      requestHeaders.set(
+        "pinata_secret_api_key",
+        `${import.meta.env.VITE_PINATA_API_SECRET}`
+      );
+
+      const resFile = await fetch(
+        "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        {
+          method: "post",
+          body: formData,
+          headers: requestHeaders,
+        }
+      );
+
+      const responseJson = await resFile.json();
+      console.log("responseJson", responseJson);
+
+      const thumbnailUri = `ipfs://${responseJson.IpfsHash}`;
+      setPictureUrl(
+        `https://gateway.pinata.cloud/ipfs/${responseJson.IpfsHash}`
+      );
+
+      const op = await nftContrat!.methods
+        .mint(
+          new BigNumber(newTokenDefinition.token_id) as nat,
+          char2Bytes(newTokenDefinition.name!) as bytes,
+          char2Bytes(newTokenDefinition.description!) as bytes,
+          char2Bytes(newTokenDefinition.symbol!) as bytes,
+          char2Bytes(thumbnailUri) as bytes
+        )
+        .send();
+
+      //close directly the form
+      setFormOpen(false);
+      enqueueSnackbar(
+        "Wine collection is minting ... it will be ready on next block, wait for the confirmation message before minting another collection",
+        { variant: "info" }
+      );
+
+      await op.confirmation(2);
+
+      enqueueSnackbar("Wine collection minted", { variant: "success" });
+
+      refreshUserContextOnPageReload(); //force all app to refresh the context
+    }
+  } catch (error) {
+    console.table(`Error: ${JSON.stringify(error, null, 2)}`);
+    let tibe: TransactionInvalidBeaconError = new TransactionInvalidBeaconError(
+      error
+    );
+    enqueueSnackbar(tibe.data_message, {
+      variant: "error",
+      autoHideDuration: 10000,
+    });
+  }
+};
+
 const validationSchema = yup.object({
   name: yup.string().required("Name is required"),
   description: yup.string().required("Description is required"),
